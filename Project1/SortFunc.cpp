8e93cb827b2199eb9d3f34d18306cc4b5ca09b28
CyberAss <sashapiter2015@gmail.com>
2020-11-22 15:42:41 +0300

    return

--- Project1/SortFunc.cpp
+++ Project1/SortFunc.cpp
@@ -27,17 +27,19 @@ bool isSorted(int arr[], int n) {
 }
 
 int binarySearch(int* arr, int l, int r, int x) {
-    if (!isSorted(arr, r)) throw "This array isn`t sorted";
-    else
-        while (l <= r) {
-            int m = l + (r - l) / 2;
-            if (arr[m] == x)
-                return m;
-            if (arr[m] < x)
-                l = m + 1;
-            else
-                r = m - 1;
-        }
+    if (!isSorted(arr, r)){
+        return -1;
+        throw "This array isn`t sorted";
+    }
+    while (l <= r) {
+        int m = l + (r - l) / 2;
+        if (arr[m] == x)
+            return m;
+        if (arr[m] < x)
+            l = m + 1;
+        else
+            r = m - 1;
+    }
     return -1;
     throw "This element doesn`t exist";
 }
